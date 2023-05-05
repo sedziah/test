d18043a93763e9f56fb9d18091ffce43e98fca60
Solomon Edziah <68519058+sedziah@users.noreply.github.com>
2023-05-05 07:00:01 +0000

    .nv file added and updated

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@p8o0rqvr9)2c*+_e)!yznfisdzrjeeu@8acuw5%4zez8fr=sj'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
